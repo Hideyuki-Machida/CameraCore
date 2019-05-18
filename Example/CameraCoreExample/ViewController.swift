4b9088d555dba20de3c97563b63a85a878bf5cac
hideyuki machida <h.machida@cchan.tv>
2019-05-18 22:39:26 +0900

    Initial commit

---
+++ Example/CameraCoreExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraCoreExample
+//
+//  Created by hideyuki machida on 2019/05/18.
+//  Copyright © 2019 hideyuki machida. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+

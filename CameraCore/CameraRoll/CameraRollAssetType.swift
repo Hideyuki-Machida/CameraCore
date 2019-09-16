a37f5d132d10f6a0161a4f894cf3782094d357d4
hideyuki machida <hideyuki.machida.jp@gmail.com>
2019-09-17 02:18:28 +0900

    update

---
+++ CameraCore/CameraRoll/CameraRollAssetType.swift
@@ -0,0 +1,23 @@
+//
+//  AssetType.swift
+//  CameraCore
+//
+//  Created by hideyuki machida on 2018/08/08.
+//  Copyright © 2018 町田 秀行. All rights reserved.
+//
+
+import Foundation
+import Photos
+
+public enum CameraRollAssetType: Int, Codable {
+    case unknown = 0
+    case image = 1
+    case video = 2
+    case audio = 3
+    
+    public var phAssetMediaType: PHAssetMediaType {
+        get {
+            return PHAssetMediaType.init(rawValue: self.rawValue)!
+        }
+    }
+}
